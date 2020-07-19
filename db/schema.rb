4dcb3f94c6fe61c075eeffd3259d0477d736db6f
Dante <motiv8lab@gmail.com>
2020-07-19 18:55:33 -0400

    implemented book enum for publish status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_19_224039) do
+ActiveRecord::Schema.define(version: 2020_07_19_224959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_07_19_224039) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_books_on_slug", unique: true
   end
 

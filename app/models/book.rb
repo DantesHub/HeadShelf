5b02476904869e6d1458feb6380de21da3fc9ccb
Dante <motiv8lab@gmail.com>
2020-07-21 20:18:20 -0400

    added custom scopes for books

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,5 +4,10 @@ class Book < ApplicationRecord
   friendly_id :title, use: :slugged
   validates_presence_of :title, :author, :body
   belongs_to :topic
+
+  def self.psychology
+    where(title: 'book')
+  end
+
+  scope :marketing_items, -> { where(topic_id: 3) }
 end
- 

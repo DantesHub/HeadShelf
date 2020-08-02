b902f27ad040ab0dc9355b4816ee46ffc5c7a0fb
Dante <motiv8lab@gmail.com>
2020-08-02 19:58:52 -0400

    started redesign

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def login_helper
     if current_user
-      link_to 'Logout', destroy_user_session_path, method: :delete
+      link_to 'Logout', destroy_user_session_path,class: 'nav-link', method: :delete
     else
-      (link_to 'Sign Up', new_user_registration_path) + '<br>'.html_safe +
-        + (link_to 'Login', new_user_session_path)
+      (link_to 'Sign Up', new_user_registration_path, class: 'nav-link') + "".html_safe +
+        + (link_to 'Login', new_user_session_path, class: 'nav-link')
       end
   end
 

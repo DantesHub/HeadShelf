6723b40590d872838a7611d8a7b6fdcd768b9165
Dante <motiv8lab@gmail.com>
2020-08-05 20:41:02 -0400

    finished implementing styles for titans and articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,18 @@
 module ApplicationHelper
   def login_helper
     if current_user
-      link_to 'Logout', destroy_user_session_path,class: 'nav-link', method: :delete
+      link_to 'Logout', destroy_user_session_path, class: 'nav-link', method: :delete
     else
-      (link_to 'Sign Up', new_user_registration_path, class: 'nav-link') + "".html_safe +
+      (link_to 'Sign Up', new_user_registration_path, class: 'nav-link') + ''.html_safe +
         + (link_to 'Login', new_user_session_path, class: 'nav-link')
       end
   end
 
   def source_helper
     content_tag(:p, "Thanks for visiting us from #{session[:source]}", class: 'source-greeting') if session[:source]
+  end
+
+  def active?(path)
+    'active' if current_page? path
   end
 end

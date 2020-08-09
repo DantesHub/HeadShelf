41d0899e9e196a09b8533cb5a5b029d5ddb73f11
Dante <motiv8lab@gmail.com>
2020-08-09 17:17:42 -0400

    updated file upload forms and refactored placeholder functionality into view

--- app/helpers/titans_helper.rb
+++ app/helpers/titans_helper.rb
@@ -1,2 +1,15 @@
 module TitansHelper
+  def image_generator(height:, width:)
+    "https://placehold.it/#{width}x#{height}"
+  end
+
+  def titan_img(img, type)
+    if img.model.thumb_image? || img.model.main_image?
+      img
+    elsif type == 'thumb'
+      image_generator(height: '325', width: '325')
+    elsif type == 'main'
+      image_generator(height: '400', width: '600')
+    end
+  end
 end

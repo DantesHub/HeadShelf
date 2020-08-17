38b21ab937c8f4646a4b471c41d202f0272da2dc
Dante <motiv8lab@gmail.com>
2020-08-16 21:07:47 -0400

    fixed social button links

--- app/assets/javascripts/demo.js
+++ app/assets/javascripts/demo.js
@@ -5,20 +5,25 @@ $.socialfloating({
       {
         icon: "facebook-f",
         enabled: !0,
-        link: "https://www.facebook.com/share?ref_src=twsrc%5Etfw",
+        link: `https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`,
         color: "#3b5998",
       },
       {
         icon: "twitter",
         enabled: !0,
-        link: "https://twitter.com/share?ref_src=twsrc%5Etfw",
+        link: `https://twitter.com/intent/tweet?url=${window.location.href}`,
         color: "#00aced",
+      },
+      {
+        icon: "linkedin",
+        enabled: !0,
+        link: `https://www.linkedin.com/shareArticle?mini=true&url=${window.location.href}&title=&summary=&source=`,
+        color: "#2774B0",
       },
       {
         icon: "pinterest",
         enabled: !0,
-        link:
-          "https://pinterest.com/pin/create/button/?url=https://www.nateliason.com/blog/30-years&media=&description=",
+        link: `https://pinterest.com/pin/create/button/?url=${window.location.href}&media=&description=`,
         color: "#811302",
       },
     ],
